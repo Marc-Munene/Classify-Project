d8baa907a460647de14a3d8acd54c1cfd78ee4d1
Marc Munene <marcmunene104@gmail.com>
2025-04-07 22:59:22 +0300

    course units

--- Server/src/controllers/courseUnit.js
+++ Server/src/controllers/courseUnit.js
@@ -79,3 +79,70 @@ export const getSingleCourse = async (req, res) => {
     });
   }
 };
+
+//edit course unit
+export const editCourseUnit = async (req, res) => {
+  try {
+    const { course, unit } = req.body;
+
+    if (!course || !unit) {
+      return res.status(400).json({
+        success: false,
+        message: "Course and Unit are required",
+      });
+    }
+    const courseUnitId = req.query.id;
+    // Find the course unit by ID and update it
+
+    const courseUnit = await CourseUnit.findByIdAndUpdate(
+      { _id: courseUnitId },
+      req.body,
+      { new: true }
+    );
+
+    if (!courseUnit) {
+      return res.status(404).json({
+        success: false,
+        message: "Course Unit not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: courseUnit,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to update course unit",
+    });
+  }
+};
+
+//delete course unit
+export const deleteCourseUnit = async (req, res) => {
+  try {
+    const courseUnitId = req.query.id;
+
+    const deletedCourseUnit = await CourseUnit.deleteOne({ _id: courseUnitId });
+
+    if (deletedCourseUnit.deletedCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Course Unit not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      deletedCount: deletedCourseUnit.deletedCount,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete course unit",
+    });
+  }
+};
--- Server/src/routes/courseUnitRoute.js
+++ Server/src/routes/courseUnitRoute.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { getCourseUnits, postCourseUnits } from "../controllers/courseUnit.js";
+import {
+  deleteCourseUnit,
+  editCourseUnit,
+  getCourseUnits,
+  postCourseUnits,
+} from "../controllers/courseUnit.js";
 
 const courseUnitRouter = Router();
 
 courseUnitRouter
   .route("/courseUnits")
   .get(getCourseUnits)
-  //   .get(getSingleCourse)
-  .post(postCourseUnits);
+  .post(postCourseUnits)
+  .put(editCourseUnit)
+  .delete(deleteCourseUnit);
 
 export { courseUnitRouter };
