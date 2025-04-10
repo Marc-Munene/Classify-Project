c0eea5e444bdf7ead4ed6050c44f15c63d3cb11a
Marc Munene <marcmunene104@gmail.com>
2025-04-10 17:45:11 +0300

    User interface

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,44 @@
-import React from "react";
-import { LogInForm } from "./components/LogInForm";
-
+import { CgProfile } from "react-icons/cg";
+import { SiGoogleclassroom } from "react-icons/si";
+import { CiBookmarkCheck } from "react-icons/ci";
+import { FaBookReader } from "react-icons/fa";
+import { FaHistory } from "react-icons/fa";
+import { CgLogOut } from "react-icons/cg";
 const App = () => {
-  return <>
-  
-  
-
-  </>;
+  return (
+    <div>
+      <div className="aside">
+        <div>
+          <span>Classes</span>
+          <SiGoogleclassroom />
+        </div>
+        <div>
+          <span>Your Bookings</span>
+          <CiBookmarkCheck />
+        </div>
+        <div>
+          <span>Booked</span>
+          <FaBookReader />
+        </div>
+        <div>
+          <span>History</span>
+          <FaHistory />
+        </div>
+        <div>
+          <span>Logout</span>
+          <CgLogOut />
+        </div>
+      </div>
+      <div className="header">
+        <h1>Find Vacant Classrooms</h1>
+        <CgProfile />
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search for classes" />
+        <button>Search</button>
+      </div>
+    </div>
+  );
 };
 
 export { App };
