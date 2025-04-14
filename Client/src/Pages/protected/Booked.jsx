7e2fc0734e233303d5a97a2a40b4dce019319e11
Marc Munene <marcmunene104@gmail.com>
2025-04-14 12:22:22 +0300

    bookings

--- Client/src/Pages/protected/Booked.jsx
+++ Client/src/Pages/protected/Booked.jsx
@@ -1,30 +1,20 @@
 const Booked = () => {
   return (
-    <div className="overflow-x-auto p-4 w-full md:w-4/5 ">
+    <div className="p-4 w-full md:w-4/5 ">
       <div className="mb-10">
         <h1 className="font-bold text-xl sm:text-2xl md:text-3xl text-center md:text-left mb-4 md:mb-0 w-full md:w-auto">
           Booked Classes
         </h1>
       </div>
-      <table className="w-full text-left text-base">
+      <table className="w-full text-left text-base overflow-hidden">
         <thead className="bg-gray-200">
           <tr>
-            <th className="p-3  hover:bg-primary hover:text-white transform transform-fill duration-300 ease-in-out hover:scale-[1.06] ">
-              ROOM
-            </th>
-            <th className="p-3 hover:bg-primary hover:text-white transform transform-fill duration-300 ease-in-out hover:scale-[1.06] ">
-              BUILDING
-            </th>
-            <th className="p-3 hover:bg-primary hover:text-white transform transform-fill duration-300 ease-in-out hover:scale-[1.06] ">
-              TIME END
-            </th>
+            <th className="p-3 text-sm md:text-xl">ROOM</th>
+            <th className="p-3 text-sm md:text-xl">BUILDING</th>
+            <th className="p-3 text-sm md:text-xl">TIME END</th>
 
-            <th className="p-3  hover:bg-primary hover:text-white transform transform-fill duration-300 ease-in-out hover:scale-[1.06] ">
-              CLASS REP
-            </th>
-            <th className="p-3  hover:bg-primary hover:text-white transform transform-fill duration-300 ease-in-out hover:scale-[1.06] ">
-              UNIT
-            </th>
+            <th className="p-3 text-sm md:text-xl ">CLASS REP</th>
+            <th className="p-3 text-sm md:text-xl ">UNIT</th>
           </tr>
         </thead>
         <tbody>
@@ -35,7 +25,7 @@ const Booked = () => {
             <td className="px-3 py-2 sm:py-3 text-sm ">NEW COHRED</td>
             <td className="px-3 py-2 sm:py-3">12:30pm</td>
             <td className="px-3 py-2 sm:py-3">MUNENE</td>
-            <td className="px-3 py-2 sm:py-3">Psychology</td>
+            <td className="px-3 py-2 sm:py-3">DMBS</td>
           </tr>
         </tbody>
       </table>
